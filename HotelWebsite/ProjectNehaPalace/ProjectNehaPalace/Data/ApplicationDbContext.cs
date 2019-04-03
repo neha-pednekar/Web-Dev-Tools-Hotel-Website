c2ae1242bf1fcb161a03111eba5742e1a72a4808
neha-pednekar <pednekar.n@husky.neu.edu>
2019-04-02 22:16:37 -0400

    DB Initial Migrations

--- HotelWebsite/ProjectNehaPalace/ProjectNehaPalace/Data/ApplicationDbContext.cs
+++ HotelWebsite/ProjectNehaPalace/ProjectNehaPalace/Data/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ProjectNehaPalace.Models;
+using ProjectNehaPalace.Models.HotelViewModels;
 
 namespace ProjectNehaPalace.Data
 {
@@ -15,12 +16,20 @@ namespace ProjectNehaPalace.Data
         {
         }
 
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Person> People { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
+
+            builder.Entity<Address>().ToTable("Address");
+            builder.Entity<Person>().ToTable("Person");
+            builder.Entity<Customer>().ToTable("Customers");
         }
     }
 }
